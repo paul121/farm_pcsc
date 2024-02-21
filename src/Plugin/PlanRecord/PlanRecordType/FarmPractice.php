8edf8e6ac0134ad715ab3dafed380992cf3114e7
Michael Stenta <mike@mstenta.net>
2024-02-21 14:22:31 -0500

    Add a PCSC Farm Practice plan record type.

---
+++ src/Plugin/PlanRecord/PlanRecordType/FarmPractice.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_pcsc\Plugin\PlanRecord\PlanRecordType;
+
+use Drupal\farm_entity\Plugin\PlanRecord\PlanRecordType\FarmPlanRecordType;
+
+/**
+ * Provides the PCSC Farm Practice plan record type.
+ *
+ * @PlanRecordType(
+ *   id = "pcsc_farm_practice",
+ *   label = @Translation("PCSC Farm Practice"),
+ * )
+ */
+class FarmPractice extends FarmPlanRecordType {
+
+}
