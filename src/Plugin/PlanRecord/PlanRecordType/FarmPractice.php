8ecb970d445eda69f4a27ead60872470cf6f0cd8
Michael Stenta <mike@mstenta.net>
2024-02-21 14:23:50 -0500

    Add a land asset reference field to farm practice records.

--- src/Plugin/PlanRecord/PlanRecordType/FarmPractice.php
+++ src/Plugin/PlanRecord/PlanRecordType/FarmPractice.php
@@ -14,4 +14,26 @@ use Drupal\farm_entity\Plugin\PlanRecord\PlanRecordType\FarmPlanRecordType;
  */
 class FarmPractice extends FarmPlanRecordType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'field' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Field'),
+        'description' => $this->t('Associates the PCSC Farm plan with a Land asset.'),
+        'target_type' => 'asset',
+        'target_bundle' => 'land',
+        'cardinality' => 1,
+        'required' => TRUE,
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
 }
