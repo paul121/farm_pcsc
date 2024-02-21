6066e546995a85eccee24a656cb4c476434c3e29
Michael Stenta <mike@mstenta.net>
2024-02-21 14:20:37 -0500

    Add PCSC Farm plan type.

---
+++ src/Plugin/Plan/PlanType/Farm.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\farm_pcsc\Plugin\Plan\PlanType;
+
+use Drupal\farm_entity\Plugin\Plan\PlanType\FarmPlanType;
+
+/**
+ * Provides the PCSC Farm plan type.
+ *
+ * @PlanType(
+ *   id = "pcsc_farm",
+ *   label = @Translation("PCSC Farm"),
+ * )
+ */
+class Farm extends FarmPlanType {
+
+}
