3652f02ae05ea7fd122ad08ea1a173a749ca206e
Michael Stenta <mike@mstenta.net>
2024-03-27 09:53:28 -0400

    Add PcscField plan_record type with field asset reference.

---
+++ src/Plugin/PlanRecord/PlanRecordType/PcscField.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\farm_pcsc\Plugin\PlanRecord\PlanRecordType;
+
+use Drupal\farm_entity\Plugin\PlanRecord\PlanRecordType\FarmPlanRecordType;
+
+/**
+ * Provides the PCSC Field plan record type.
+ *
+ * @PlanRecordType(
+ *   id = "pcsc_field",
+ *   label = @Translation("PCSC Field"),
+ * )
+ */
+class PcscField extends FarmPlanRecordType {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+    $field_info = [
+      'field' => [
+        'type' => 'entity_reference',
+        'label' => $this->t('Field'),
+        'description' => $this->t('Associates the PCSC Farm plan with a Land asset.'),
+        'target_type' => 'asset',
+        'target_bundle' => 'land',
+        'cardinality' => 1,
+        'required' => TRUE,
+      ],
+    ];
+    foreach ($field_info as $name => $info) {
+      $fields[$name] = $this->farmFieldFactory->bundleFieldDefinition($info);
+    }
+    return $fields;
+  }
+
+}
