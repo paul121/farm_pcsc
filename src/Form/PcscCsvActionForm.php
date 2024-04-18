2423db180b11ad31f6cda236fcc1e2594307d426
Paul Weidner <paul.weidner@gmail.com>
2024-04-17 23:08:48 -0700

    Add CSV export action

---
+++ src/Form/PcscCsvActionForm.php
@@ -0,0 +1,311 @@
+<?php
+
+namespace Drupal\farm_pcsc\Form;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\Core\File\FileUrlGeneratorInterface;
+use Drupal\Core\Form\ConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
+use Drupal\Core\Url;
+use Drupal\file\FileRepositoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+
+/**
+ * Provides a PCSC CSV action form.
+ *
+ * @see \Drupal\farm_export_csv\Plugin\Action\EntityCsv
+ * @see \Drupal\Core\Entity\Form\DeleteMultipleForm
+ */
+class PcscCsvActionForm extends ConfirmFormBase {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $user;
+
+  /**
+   * The tempstore factory.
+   *
+   * @var \Drupal\Core\TempStore\SharedTempStore
+   */
+  protected $tempStore;
+
+  /**
+   * The serializer service.
+   *
+   * @var \Symfony\Component\Serializer\SerializerInterface
+   */
+  protected $serializer;
+
+  /**
+   * The file system service.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
+   * The file repository service.
+   *
+   * @var \Drupal\file\FileRepositoryInterface
+   */
+  protected $fileRepository;
+
+  /**
+   * The file URL generator.
+   *
+   * @var \Drupal\Core\File\FileUrlGeneratorInterface
+   */
+  protected $fileUrlGenerator;
+
+  /**
+   * The entities to export.
+   *
+   * @var \Drupal\Core\Entity\EntityInterface[]
+   */
+  protected $entities;
+
+  /**
+   * Constructs an PcscCsvActionForm form object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Session\AccountInterface $user
+   *   The current user.
+   * @param \Drupal\Core\TempStore\PrivateTempStoreFactory $temp_store_factory
+   *   The tempstore factory.
+   * @param \Symfony\Component\Serializer\SerializerInterface $serializer
+   *   The serializer service.
+   * @param \Drupal\Core\File\FileSystemInterface $file_system
+   *   The file system service.
+   * @param \Drupal\file\FileRepositoryInterface $file_repository
+   *   The file repository service.
+   * @param \Drupal\Core\File\FileUrlGeneratorInterface $file_url_generator
+   *   The file URL generator.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, AccountInterface $user, PrivateTempStoreFactory $temp_store_factory, SerializerInterface $serializer, FileSystemInterface $file_system, FileRepositoryInterface $file_repository, FileUrlGeneratorInterface $file_url_generator) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->user = $user;
+    $this->tempStore = $temp_store_factory->get('pcsc_csv_confirm');
+    $this->serializer = $serializer;
+    $this->fileSystem = $file_system;
+    $this->fileRepository = $file_repository;
+    $this->fileUrlGenerator = $file_url_generator;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('current_user'),
+      $container->get('tempstore.private'),
+      $container->get('serializer'),
+      $container->get('file_system'),
+      $container->get('file.repository'),
+      $container->get('file_url_generator'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'farm_pcsc_csv_action_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->formatPlural(count($this->entities), 'Export a CSV of @count item?', 'Export a CSV of @count items?');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelUrl() {
+    return Url::fromUri('plans/pcsc_producer');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription() {
+    return 'This will export a CSV in a format that is compatible with PCSC workbook sheets.';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Export');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $entity_type_id = NULL) {
+
+    // Load entities from the temp store and filter out ones that the user
+    // doesn't have access to.
+    $this->entities = [];
+    $inaccessible_entities = [];
+    foreach ($this->tempStore->get($this->user->id()) as $entity) {
+      if (!$entity->access('view', $this->currentUser())) {
+        $inaccessible_entities[] = $entity;
+        continue;
+      }
+      $this->entities[] = $entity;
+    }
+
+    // Add warning message for inaccessible entities.
+    if (!empty($inaccessible_entities)) {
+      $inaccessible_count = count($inaccessible_entities);
+      $this->messenger()->addWarning($this->formatPlural($inaccessible_count, 'Can not export @count item because you do not have the necessary permissions.', 'Can not export @count items because you do not have the necessary permissions.'));
+    }
+
+    // If we don't have a list of entities, redirect.
+    $this->entities = $this->tempStore->get($this->user->id());
+    if (empty($this->entities)) {
+      return new RedirectResponse($this->getCancelUrl()->setAbsolute()->toString());
+    }
+
+    // Delegate to the parent method.
+    $form = parent::buildForm($form, $form_state);
+
+    $form['sheet_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Sheet Type'),
+      '#description' => $this->t('Select the type of workbook sheet to export.'),
+      '#options' => [
+        'all' => $this->t('All'),
+        'enrollment' => $this->t('Enrollment'),
+        'summary' => $this->t('Summary'),
+        'supplemental' => $this->t('Supplemental'),
+      ],
+      '#empty_option' => $this->t('Select one'),
+      '#required' => TRUE,
+    ];
+
+    $form['enrollment_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Enrollment sheet'),
+      '#description' => $this->t('Select the enrollment sheet to export.'),
+      '#options' => [
+        'producer' => $this->t('Producer Enrollment'),
+        'field' => $this->t('Field Enrollment'),
+      ],
+      '#states' => [
+        'visible' => [
+          'select[name="sheet_type"]' => ['value' => 'enrollment'],
+        ],
+        'required' => [
+          'select[name="sheet_type"]' => ['value' => 'enrollment'],
+        ],
+      ],
+    ];
+
+    $form['summary_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Summary sheet'),
+      '#description' => $this->t('Select the summary sheet to export.'),
+      '#options' => [
+        'producer' => $this->t('Producer Summary'),
+        'field' => $this->t('Field Summary'),
+      ],
+      '#states' => [
+        'visible' => [
+          'select[name="sheet_type"]' => ['value' => 'summary'],
+        ],
+        'required' => [
+          'select[name="sheet_type"]' => ['value' => 'summary'],
+        ],
+      ],
+    ];
+
+    $form['supplemental_type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Supplemental sheet'),
+      '#description' => $this->t('Select the supplemental sheet to export.'),
+      '#options' => [
+        '528' => $this->t('Prescribed Grazing (528)'),
+      ],
+      '#states' => [
+        'visible' => [
+          'select[name="sheet_type"]' => ['value' => 'supplemental'],
+        ],
+        'required' => [
+          'select[name="sheet_type"]' => ['value' => 'supplemental'],
+        ],
+      ],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    // @TODO build data.
+    $data = [];
+
+    // Serialize the data as CSV.
+    $output = $this->serializer->serialize($data, 'csv');
+
+    // Prepare the file directory.
+    $directory = 'private://csv';
+    $this->fileSystem->prepareDirectory($directory, FileSystemInterface::CREATE_DIRECTORY);
+
+    // Create the file.
+    $date = date('c');
+    $filename = "$sheet_type-$date.csv";
+    $destination = "$directory/$filename";
+    try {
+      $file = $this->fileRepository->writeData($output, $destination);
+    }
+
+    // If file creation failed, bail with a warning.
+    catch (\Exception $e) {
+      $this->messenger()->addWarning($this->t('Could not create file.'));
+      return;
+    }
+
+    // Make the file temporary.
+    $file->status = 0;
+    $file->save();
+
+    // Add confirmation message.
+    if (count($this->entities)) {
+      $this->messenger()->addStatus($this->formatPlural(count($this->entities), 'Exported @count item.', 'Exported @count items'));
+    }
+
+    // Show a link to the file.
+    $url = $this->fileUrlGenerator->generateAbsoluteString($file->getFileUri());
+    $this->messenger()->addMessage($this->t('CSV file created: <a href=":url">%filename</a>', [
+      ':url' => $url,
+      '%filename' => $file->label(),
+    ]));
+
+    // Clean up the temporary storage.
+    $this->tempStore->delete($this->currentUser()->id());
+  }
+
+}
---
+++ src/Plugin/Action/PcscCsv.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Drupal\farm_pcsc\Plugin\Action;
+
+use Drupal\Core\Action\Plugin\Action\EntityActionBase;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Action that exports CSV files for use in PCSC workbooks.
+ *
+ * @Action(
+ *   id = "farm_pcsc:csv_action",
+ *   action_label = @Translation("Export PCSC CSV"),
+ *   type = "plan",
+ *   confirm_form_route_name = "farm_pcsc.csv_action_form",
+ * )
+ */
+class PcscCsv extends EntityActionBase {
+
+  /**
+   * The tempstore object.
+   *
+   * @var \Drupal\Core\TempStore\SharedTempStore
+   */
+  protected $tempStore;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new PcscCsv object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   * @param \Drupal\Core\TempStore\PrivateTempStoreFactory $temp_store_factory
+   *   The tempstore factory.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   Current user.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PrivateTempStoreFactory $temp_store_factory, AccountInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager);
+    $this->tempStore = $temp_store_factory->get('pcsc_csv_confirm');
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('tempstore.private'),
+      $container->get('current_user'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function executeMultiple(array $entities) {
+    /** @var \Drupal\Core\Entity\EntityInterface[] $entities */
+    $this->tempStore->set($this->currentUser->id(), $entities);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($object = NULL) {
+    $this->executeMultiple([$object]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    return $object->access('view', $account, $return_as_object);
+  }
+
+}
