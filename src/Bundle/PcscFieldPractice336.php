1ba028d40eacad18f938d73c46d161777ef4b022
Michael Stenta <mike@mstenta.net>
2024-05-09 11:14:37 -0400

    Add practice 336.

---
+++ src/Bundle/PcscFieldPractice336.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Drupal\farm_pcsc\Bundle;
+
+/**
+ * Provides the PCSC Field Practice 336 bundle class.
+ * )
+ */
+class PcscFieldPractice336 extends PcscFieldPracticeBase {
+
+}
---
+++ src/Plugin/PlanRecord/PlanRecordType/PcscFieldPractice336.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\farm_pcsc\Plugin\PlanRecord\PlanRecordType;
+
+/**
+ * Provides the PCSC Field Practice 336 plan record type.
+ *
+ * @PlanRecordType(
+ *   id = "pcsc_field_practice_336",
+ *   label = @Translation("PCSC Field Practice 336"),
+ * )
+ */
+class PcscFieldPractice336 extends PcscFieldPracticeBase {
+
+}
