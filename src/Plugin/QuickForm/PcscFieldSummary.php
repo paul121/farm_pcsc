c145e566f3c2a0adde50f0cc22070205b2942423
Paul Weidner <paul.weidner@gmail.com>
2024-05-31 15:37:14 -0700

    Drop enrollment text from farm and field summary year/quarter

--- src/Plugin/QuickForm/PcscFieldSummary.php
+++ src/Plugin/QuickForm/PcscFieldSummary.php
@@ -87,14 +87,14 @@ class PcscFieldSummary extends QuickFormBase {
     $form['enrollment'] = $this->buildInlineContainer();
     $form['enrollment']['pcsc_year'] = [
       '#type' => 'select',
-      '#title' => $this->t('Enrollment year'),
+      '#title' => $this->t('Year'),
       '#options' => farm_pcsc_allowed_values_helper([2024, 2025, 2026, 2027, 2028]),
       '#default_value' => date('Y'),
       '#required' => TRUE,
     ];
     $form['enrollment']['pcsc_quarter'] = [
       '#type' => 'select',
-      '#title' => $this->t('Enrollment quarter'),
+      '#title' => $this->t('Quarter'),
       '#options' => farm_pcsc_allowed_values_helper([1, 2, 3, 4]),
       '#default_value' => ceil(date('m') / 3),
       '#required' => TRUE,
